a1fd37578af466085bff2d0942c17d91ec0b881e
BigForLy <oringan@mail.ru>
2021-12-04 20:08:19 +0500

    #4

--- shared/src/androidMain/kotlin/com/example/qrfamily/QrGenerator.kt
+++ shared/src/androidMain/kotlin/com/example/qrfamily/QrGenerator.kt
@@ -12,8 +12,6 @@ class QrGenerator {
         // https://stackoverflow.com/questions/64443791/android-qr-generator-api
         val size = 512 //pixels
         val qrCodeContent = "https://ktor.io/docs/getting-started-ktor-client.html"
-//        val qrCodeContent = "WIFI:S:$ssid;T:WPA;P:$password;;"
-//        val hints = hashMapOf<EncodeHintType, Int>().also { it[EncodeHintType.MARGIN] = 1 } // Make the QR code buffer border narrower
         val bits = QRCodeWriter().encode(qrCodeContent, BarcodeFormat.QR_CODE, size, size)
         return Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
             for (x in 0 until size) {
--- shared/src/commonMain/kotlin/com/example/qrfamily/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/qrfamily/Greeting.kt
@@ -1,13 +1,7 @@
 package com.example.qrfamily
 
-//import io.ktor.client.*
-//import io.ktor.client.features.*
-//import io.ktor.client.request.*
-
 class Greeting {
     fun greeting(): String {
-//        val client = HttpClient()
-//        val response = client.get<String>("https://ktor.io/")
         return "Hello, ${Platform().platform}!"
     }
 }
